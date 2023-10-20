b88a370d72c38303f1417f681781f754d64f8e27
Youngseok Joung <youngseok.joung@lge.com>
2023-10-20 16:20:10 +0900

    add_

--- src/view/Pages/Review/Bookmark.js
+++ src/view/Pages/Review/Bookmark.js
@@ -1,4 +1,7 @@
 import React, {useState} from "react"
+import { useSelector, shallowEqual } from 'react-redux';
+import { Spin } from 'antd';
+
 import BookmarkedList from '../../Component/List/BookmarkedList'
 import BookmarkModal from "../../Component/Modal/BookmarkModal"
 
@@ -8,9 +11,16 @@ export default function Bookmark () {
 
     const closeBookmarkModal = () => {setIsBookmarkModalOpen(false)};
     
+    const { isLoading } = useSelector((state) => {
+        let isLoading = state.data.isLoading
+        return { 
+          isLoading: isLoading
+        }
+      }, shallowEqual)
+
     return (
         <>
-        <BookmarkedList onItemClicked={()=>setIsBookmarkModalOpen(true)} setModalContent={setModalContent}/>
+        isLoading ? <Spin /> : <BookmarkedList onItemClicked={()=>setIsBookmarkModalOpen(true)} setModalContent={setModalContent}/>
         <BookmarkModal open={isBookmarkModalOpen} onClosed={closeBookmarkModal} modalContent = {modalContent}/>
         </>
     )
