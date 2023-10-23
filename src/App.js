9846af3f203ede6807f5e2283395a04fa119f4de
Youngseok Joung <youngseok.joung@lge.com>
2023-10-23 20:51:40 +0900

    add

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { useState } from 'react'
 import store from "./store"
 import { Provider } from "react-redux"
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+import PrivateScreen from './view/Component/Login/screens/PrivateScreen'
+import LoginScreen from './view/Component/Login/screens/LoginScreen'
+import RegisterScreen from './view/Component/Login/screens/RegisterScreen'
+import ForgotPasswordScreen from './view/Component/Login/screens/ForgotPasswordScreen'
+import ResetPasswordScreen from './view/Component/Login/screens/ResetPasswordScreen'
+
 import Main from './view/Pages/Main'
 import Bookmark from './view/Pages/Review/Bookmark';
 import History from './view/Pages/Review/History';
-import TestPage from './view/Pages/TestPage'
 import Reference from './view/Pages/Reference'
+
+
 
 import Layout from './view/Layout/Layout';
 import Recommend from './view/Pages/Recommend'
